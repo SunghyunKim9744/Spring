24e3cdf0a6fe0830b9e918f6a4a2ed8509077579
SunghyunKim9744 <dqq9744@naver.com>
2020-12-31 09:39:41 +0900

    12/30 Tiles

--- src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -1,10 +1,10 @@
-package com.newlecture.web.controller.customer.board;
+package com.newlecture.web.controller.customer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/customer/board/notice/")
+@RequestMapping("/customer/notice/")
 public class NoticeController {
 
 	@RequestMapping("list")
@@ -14,7 +14,16 @@ public class NoticeController {
 //		return "list.jsp";
 		
 //		return "/WEB-INF/view/customer/board/notice/list.jsp";
-		return "/customer/board/notice/list";
+//		return "customer/notice/list";
+//		tiles가 인식할 수 있는 이름으로 바꾸기
+	    return "customer.notice.list";
+	}
+	
+	@RequestMapping("detail")
+	public String detail() {
+		return "customer.notice.detail";
 	}
 
 }
+
+
