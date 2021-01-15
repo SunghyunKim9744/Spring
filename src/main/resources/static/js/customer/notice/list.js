28aa19f86d3ff0043949d06018439f1532fa5467
SunghyunKim9744 <dqq9744@naver.com>
2021-01-15 11:44:36 +0900

    1/15 프론트에서의 데이터 요청(Ajax)

--- src/main/resources/static/js/customer/notice/list.js
+++ src/main/resources/static/js/customer/notice/list.js
@@ -1,12 +1,15 @@
 window.addEventListener("load",(e)=>{
 	const pager = document.querySelector(".pager");
 
-	
+	const notice = document.querySelector(".notice");
+	const tbody = notice.querySelector("tbody");
+	const searchForm = document.querySelector(".search-form");
+	const searchButton = searchForm.querySelector(".btn-search");
+	const queryInput = searchForm.querySelector("input[name=q]");
 	pager.addEventListener("click",(e)=>{
 		e.preventDefault();
 		console.log(e.target.innerText);
-		
-		
+
 		// ==== 옛날 방식
 		/*
 		let request = new XMLHttpRequest();
@@ -16,10 +19,56 @@ window.addEventListener("load",(e)=>{
 		request.open("GET","/api/notice/list");
 		request.send();
 		*/
-		
 		// ==== 현재 방식
-		fetch("/api/notice/list")
-		.then(response =>console.log(response));
+		let page = e.target.innerText;
+		let query = queryInput.value;
+		let field="title";
+		fetch(`/api/notice/list?p=${page}&f=${field}&q=${query}`)
+		.then(response =>response.json())
+		.then(
+			json=>{
+				tbody.innerHTML="";
+				
+				for(let n of json){
+					let tr = `
+						<td>${n.id}</td>
+						<td class="title indent text-align-left"><a href="${n.id}">${n.title}</a>[${n.cmtCount}]</td>
+						<td>${n.writerId}</td>
+						<td>${n.regdate}"</td>
+						<td>${n.hit }</td>
+					`;
+					tbody.insertAdjacentHTML("beforeend",tr);
+				}
+			
+			}
+		);
+	});
+	
+	searchButton.addEventListener("click",(e)=>{
+		e.preventDefault();
+
+		let page = e.target.innerText;
+		let query = queryInput.value;
+		let field="title";
+		fetch(`/api/notice/list?p=${page}&f=${field}&q=${query}`)
+		.then(response =>response.json())
+		.then(
+			json=>{
+				tbody.innerHTML="";
+				
+				for(let n of json){
+					let tr = `
+						<td>${n.id}</td>
+						<td class="title indent text-align-left"><a href="${n.id}">${n.title}</a>[${n.cmtCount}]</td>
+						<td>${n.writerId}</td>
+						<td>${n.regdate}"</td>
+						<td>${n.hit }</td>
+					`;
+					tbody.insertAdjacentHTML("beforeend",tr);
+				}
+			
+			}
+		);
 
 	});
 });
